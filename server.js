cc0a77e5744a07abe2751e8a8ab768439d884339
Vijay <itsvijay111@gmail.com>
2016-01-08 01:41:03 +0000

    Added first todo GET method

--- server.js
+++ server.js
@@ -2,10 +2,29 @@ var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
 
+var todos = [{
+     id: 1,
+     description: 'Learn todo api using node.js...',
+     completed: 'false'
+   },{
+     id: 2,
+     description: 'Learn Data Structures and Algorithms...',
+     completed: 'false'
+   },{
+       id: 3,
+       description: 'Learn Javascript the iwerd parts...',
+       completed: 'true'
+   }
+]
+
 app.get('/', function(req,res) {
     res.send('Todo API  welcomes you!!!');
-})
+});
+
+app.get('/todos', function(req, res) {
+   res.json(todos); 
+});
 
 app.listen(port, function(req,res) {
     console.log("Express Todo web server started...");
-})
+});
