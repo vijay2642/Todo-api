1719aacd067a90fe53e3c876c4f3dd1c097475fd
Vijay <itsvijay111@gmail.com>
2016-01-14 06:00:07 +0000

    added db.js

--- db.js
+++ db.js
@@ -1,27 +1,21 @@
-
-var env = process.env.NODE_ENV || 'development';
 var Sequelize = require('sequelize');
+var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-if(env === 'production') {
-    sequelize = new sequelize(process.env.DATABASE_URL, {
-        'dialect' : 'postgresql'
-    });
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
 } else {
-    sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/todo-dev-api.sqlite'
-});
+	sequelize = new Sequelize(undefined, undefined, undefined, {
+		'dialect': 'sqlite',
+		'storage': __dirname + '/data/dev-todo-api.sqlite'
+	});
 }
 
-
-// var sequelize = new Sequelize(undefined, undefined, undefined, {
-//     'dialect': 'sqlite',
-//     'storage': __dirname + '/data/todo-dev-api.sqlite'
-// });
-
 var db = {};
-db.Todo = sequelize.import(__dirname+'/models/todo.js');
+
+db.todo = sequelize.import(__dirname + '/models/todo.js');
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
