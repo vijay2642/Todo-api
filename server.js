a6c3f2d999c3195a09c03a1d133d89cc46548793
Vijay <itsvijay111@gmail.com>
2016-01-15 03:35:11 +0000

    Removed old code in Server.js

--- server.js
+++ server.js
@@ -5,21 +5,6 @@ var app = express();
 var port = process.env.PORT || 3000;
 var db = require('./db.js');
 
-var todos1 = [{
-    id: 1,
-    description: 'Learning todo api using node.js...',
-    completed: 'false'
-}, {
-    id: 2,
-    description: 'will Learn Data Structures and Algorithms...',
-    completed: 'false'
-}, {
-    id: 3,
-    description: 'Learned Javascript the iwerd parts...',
-    completed: 'true'
-}]
-
-var todoId = 1;
 
 app.use(bodyparser.json());
 
@@ -130,35 +115,6 @@ app.put('/todos/:id', function(req, res) {
             res.status(404).send();
         }
     })
-    
-    
-    // var matchedbody = _.findWhere(todos1, {
-    //     id: todoId
-    // });
-    // var validProperties = {};
-
-    // if (!matchedbody) {
-    //     res.status(404).send();
-    // }
-
-    // if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-    //     validProperties.completed = body.completed;
-    // }
-    // else if (body.hasOwnProperty('completed')) {
-    //     return res.status(400).send();
-    // }
-
-    // if (body.hasOwnProperty('description') && _.isString(body.description)) {
-    //     validProperties.description = body.description;
-    // }
-    // else if (body.hasOwnProperty('description')) {
-    //     return res.status(400).send();
-    // }
-
-    // _.extend(matchedbody, validProperties);
-    // res.send('Values updated');
-    
-    
 });
 
 db.sequelize.sync().then(function() {
